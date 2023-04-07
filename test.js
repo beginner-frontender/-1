9c752d76c8f157efd4956ed0dcd3f0be75351e0b
beginner-frontender <git@github.com:beginner-frontender/green-corp-landing.git>
2023-04-07 23:30:02 +0400

    /

--- test.js
+++ test.js
@@ -47,8 +47,16 @@ like.addEventListener("click", e => {
     }
 })
 
+// корзина
+const trash = document.createElement("i");
+trash.className = "fa-solid fa-trash card__trash";
+trash.addEventListener("click", e => {
+    e.stopPropagation();
+    deleteCard(cat.id, card);
+})
+
 // Добавление в карточку
-card.append(like, name);
+card.append(like, name, trash);
 if (cat.age >= 0) {
     const age = document.createElement("span");
     age.innerText = cat.age;
@@ -113,21 +121,76 @@ function addCat(cat) {
     .then(data => {
         // console.log(data);
     })
+    
 }
-// addCat(cat);
+addCat(cat);
 
 // функция удаления карточки с котом 
-// function deleteCard(id, el) {
-//     if (id) {
-//         fetch(`${path}/delete/${id}`, {
-//             method: "delete"
-//         })
-//         .then(res => {
-//             if (res.status === 200) {
-//                 el.remove();
-//             }
-//         })
-//     }
-// }
+function deleteCard(id, el) {
+    if (id) {
+        fetch(`${path}/delete/${id}`, {
+            method: "delete"
+        })
+        .then(res => {
+            if (res.status === 200) {
+                el.remove();
+            }
+        })
+    }
+}
 
+// Открытие и закрытие модального окна
+const addBtn = document.querySelector(".addBtn");
+const mdBox = document.querySelector(".modal-container");
+const mdClose = mdBox.querySelector(".modal-close");
+
+addBtn.addEventListener("click", e => {
+    mdBox.style.display = "flex";
+});
+mdClose.addEventListener("click", e => {
+    mdBox.style = null;
+});
+// ----------------------------------------------
+// Отправка формы обратной связи
+const addForm = document.forms.add;
+addForm.addEventListener("submit", e => {
+    e.preventDefault(); // остановить действие по умолчанию
+    const body = {};
+    // console.log(addForm.children) // дочерние теги (прямые потомки)
+    // console.log(addForm.elements) // все элементы формы (input/select/textarea/button)
+
+    for (let i = 0; i < addForm.elements.length; i++) {
+        const inp = addForm.elements[i];
+        if (inp.name) { // элемент формы имеет атрибут name (не является кнопкой)
+            if (inp.type === "checkbox") {
+                body[inp.name] = inp.checked;
+            } else {
+                body[inp.name] = inp.value;
+            }
+        }
+    }
+// отправка формы в базу данных
+    fetch(path + "/add", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    .then(res => {
+        if (res.ok) {
+            addForm.reset();  // очистить форму
+            mdBox.style = null; //закрыть форму
+            createCart(body); //отправка в тело бади без перезагрузки страницы
+        } else {
+            return res.json();
+        }
+    })
+    .then(err => {
+        if (err && err.message) {
+            alert(err.message); //вывести сообщение об ошибке
+        }
+    });
+})
+// -------------------------------------------------------
 
